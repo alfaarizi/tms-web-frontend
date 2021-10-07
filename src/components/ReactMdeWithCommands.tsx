4b107a7d6d8dab493917487298e7f24f52661c27
Péter Kaszab <9779385-peterkaszab@users.noreply.gitlab.com>
2021-10-07 21:55:10 +0200

    refactor: use the same stylesheet for all markdown content and simplify markdown editor components

--- src/components/ReactMdeWithCommands.tsx
+++ src/components/ReactMdeWithCommands.tsx
@@ -1,15 +1,19 @@
-/* eslint react/destructuring-assignment: 0 */
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import ReactMde, { Command, CommandContext, ExecuteOptions } from 'react-mde';
-import { ReactMdeProps } from 'react-mde/lib/definitions/components/ReactMde';
 import { useTranslation } from 'react-i18next';
 import { faImages } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import 'react-mde/lib/styles/css/react-mde-all.css';
-
 import { useShow } from 'ui-hooks/useShow';
+import { MarkdownRenderer } from 'components/MarkdownRenderer/MarkdownRenderer';
+
+// Import all react-mde styles expect react-mde-preview
+// The MarkdownRenderer component will import the stylesheet for markdown content
+import 'react-mde/lib/styles/css/react-mde-toolbar.css';
+import 'react-mde/lib/styles/css/react-mde-editor.css';
+import 'react-mde/lib/styles/css/react-mde.css';
+import 'react-mde/lib/styles/css/react-mde-suggestions.css';
 
 interface InsertImageCommandContext extends CommandContext {
     type: 'insert-image';
@@ -31,16 +35,21 @@ const insertImageCommand: Command = {
 
 export type InsertFunc = (url: string) => void;
 
-interface Props extends ReactMdeProps {
-    renderGallery?: (insertFunc: InsertFunc) => React.ReactNode
+interface Props {
+    value: string;
+    onChange: (value: string) => void;
+    renderGallery?: (insertFunc: InsertFunc) => React.ReactNode;
 }
 
 /**
- * This is a workaround for inserting images from an external gallery
+ * This component extends ReactMde with custom commands
+ * Also, it includes a workaround for inserting images from an external gallery
  * @param props
  * @constructor
  */
-export function ReactMdeWithCommands(props: Props) {
+export function ReactMdeWithCommands({ value, onChange, renderGallery }: Props) {
+    const [selectedTab, setSelectedTab] = useState<'write' | 'preview'>('write');
+
     // We will access ReactMde editor with a ref
     const mdeRef = useRef<any>(null);
     const {
@@ -66,15 +75,23 @@ export function ReactMdeWithCommands(props: Props) {
         <>
             {/* Pass original props to ReactMde, and add insert-image command */}
             <ReactMde
-                {...props}
+                value={value}
+                onChange={onChange}
                 l18n={{
                     write: t('reactMde.write'),
                     preview: t('reactMde.preview'),
                     uploadingImage: t('reactMde.uploadingImage'),
                     pasteDropSelect: t('reactMde.pasteDropSelect'),
                 }}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) => Promise.resolve(<MarkdownRenderer source={markdown} />)}
+                childProps={{
+                    writeButton: {
+                        tabIndex: -1,
+                    },
+                }}
                 commands={{
-                    ...props.commands,
                     'insert-image': insertImageCommand,
                 }}
                 ref={(c) => {
@@ -82,7 +99,7 @@ export function ReactMdeWithCommands(props: Props) {
                 }}
             />
 
-            {props.renderGallery
+            {renderGallery
                 ? (
                     <div className="my-1 py-1 border-bottom">
                         {/* Render show gallery button */}
@@ -93,7 +110,7 @@ export function ReactMdeWithCommands(props: Props) {
                         </Button>
 
                         {/* Render gallery and pass handleImageInsert function */}
-                        {show && props.renderGallery(handleImageInsert)}
+                        {show && renderGallery(handleImageInsert)}
                     </div>
                 ) : null}
         </>
