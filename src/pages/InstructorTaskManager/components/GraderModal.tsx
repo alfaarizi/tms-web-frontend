d39c8bd94034a0bc74e21ba22d49b7c8d9bd6e20
Szabi Fábián <g6k9q9@inf.elte.hu>
2022-10-18 19:57:37 +0000

    fix: Grading window closes too easily

--- src/pages/InstructorTaskManager/components/GraderModal.tsx
+++ src/pages/InstructorTaskManager/components/GraderModal.tsx
@@ -1,10 +1,11 @@
 import { useForm } from 'react-hook-form';
 import { Form, Modal } from 'react-bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { StudentFile } from 'resources/instructor/StudentFile';
 import { FormButtons } from 'components/Buttons/FormButtons';
+import { ConfirmModal } from 'components/Modals/ConfirmModal';
 
 type Props = {
     file: StudentFile | null;
@@ -20,11 +21,15 @@ export function GraderModal({
     onSave,
 }: Props) {
     const { t } = useTranslation();
+    const [confirmDialog, setConfirmDialog] = useState(false);
     const {
         register,
         handleSubmit,
         setValue,
         watch,
+        reset,
+        getValues,
+        formState,
     } = useForm<StudentFile>();
 
     const onSubmit = handleSubmit((data) => {
@@ -39,6 +44,7 @@ export function GraderModal({
     // Bootstrap modal onShow event handler
     // Sets field values
     const handleShow = () => {
+        reset(getValues());
         if (file) {
             if (['Accepted', 'Passed'].includes(file.isAccepted)) {
                 setValue('isAccepted', 'Accepted');
@@ -55,56 +61,83 @@ export function GraderModal({
     };
     const selectValue = watch('isAccepted');
 
+    const handleGraderExiting = () => {
+        if (formState.isDirty || (Object.keys(formState.dirtyFields).length !== 0)) {
+            setConfirmDialog(true);
+        } else {
+            onCancel();
+        }
+    };
+
+    const onConfirm = () => {
+        setConfirmDialog(false);
+        onCancel();
+    };
+
     // Render
     return (
-        <Modal show={show} onHide={onCancel} animation size="lg" onShow={handleShow}>
-            <Modal.Header closeButton>
-                <Modal.Title>{t('task.grade')}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={onSubmit}>
-                    <Form.Group>
-                        <Form.Label>
-                            {t('task.status')}
-                            :
-                        </Form.Label>
-                        <Form.Control
-                            as="select"
-                            value={selectValue}
-                            {...register('isAccepted', { required: true })}
-                            size="sm"
-                        >
-                            <option value="Accepted">{t('status.accepted')}</option>
-                            <option value="Rejected">{t('status.rejected')}</option>
-                            <option value="Late Submission">{t('status.lateSubmission')}</option>
+        <>
+            <Modal
+                show={show}
+                animation
+                size="lg"
+                onShow={handleShow}
+                onHide={handleGraderExiting}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>{t('task.grade')}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={onSubmit}>
+                        <Form.Group>
+                            <Form.Label>
+                                {t('task.status')}
+                                :
+                            </Form.Label>
+                            <Form.Control
+                                as="select"
+                                value={selectValue}
+                                {...register('isAccepted', { required: true })}
+                                size="sm"
+                            >
+                                <option value="Accepted">{t('status.accepted')}</option>
+                                <option value="Rejected">{t('status.rejected')}</option>
+                                <option value="Late Submission">{t('status.lateSubmission')}</option>
 
-                        </Form.Control>
-                    </Form.Group>
+                            </Form.Control>
+                        </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>
-                            {t('task.notes')}
-                            :
-                        </Form.Label>
-                        <Form.Control as="textarea" {...register('notes', { required: false })} size="sm" />
-                    </Form.Group>
+                        <Form.Group>
+                            <Form.Label>
+                                {t('task.notes')}
+                                :
+                            </Form.Label>
+                            <Form.Control as="textarea" {...register('notes', { required: false })} size="sm" />
+                        </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>
-                            {t('task.grade')}
-                            :
-                        </Form.Label>
-                        <Form.Control
-                            type="number"
-                            size="sm"
-                            step="0.01"
-                            {...register('grade', { required: false })}
-                        />
-                    </Form.Group>
-
-                    <FormButtons onCancel={onCancel} />
-                </Form>
-            </Modal.Body>
-        </Modal>
+                        <Form.Group>
+                            <Form.Label>
+                                {t('task.grade')}
+                                :
+                            </Form.Label>
+                            <Form.Control
+                                type="number"
+                                size="sm"
+                                step="0.01"
+                                {...register('grade', { required: false })}
+                            />
+                        </Form.Group>
+                        <FormButtons onCancel={handleGraderExiting} />
+                    </Form>
+                </Modal.Body>
+            </Modal>
+            <ConfirmModal
+                description={t('common.confirmDiscard')}
+                isConfirmDialogOpen={confirmDialog}
+                onCancel={() => { setConfirmDialog(false); }}
+                onConfirm={onConfirm}
+                title={t('common.areYouSure')}
+            />
+        </>
     );
 }
