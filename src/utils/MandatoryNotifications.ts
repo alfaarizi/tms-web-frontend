f064a507630ca066534bd979365693a56d22d94e
László Kőrösi <klackooj@gmail.com>
2024-05-26 11:18:47 +0000

    feat: Handle misconfigured system clock on client machine

---
+++ src/utils/MandatoryNotifications.ts
@@ -0,0 +1,10 @@
+import { TFunction } from 'react-i18next';
+import { Notification } from '../resources/common/Notification';
+
+export function getClockDifferenceNotification(diff: number, t: TFunction<'translation'>): Notification {
+    return {
+        id: -1,
+        message: t('errorPage.serverTimeDiff', { diff }),
+        dismissable: false,
+    };
+}
