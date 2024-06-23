709efe21f910268c1ae57e80c18a2b1034eaf40f
Máté Cserép <mcserep@inf.elte.hu>
2024-06-23 15:47:22 +0200

    feat: Added Frontend and Backend version to About page.
    Refactor content of About page.

--- src/pages/AboutPage/components/ProjectInformation.tsx
+++ src/pages/AboutPage/components/ProjectInformation.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { usePublicSystemInfoQuery } from 'hooks/common/SystemHooks';
 import { useTranslation } from 'react-i18next';
 
 export function ProjectInformation() {
+    const publicSystemInfo = usePublicSystemInfoQuery();
     const { t } = useTranslation();
 
     return (
@@ -9,48 +11,15 @@ export function ProjectInformation() {
             <p>
                 {t('aboutPage.description')}
             </p>
-            <h2>{t('aboutPage.repositories')}</h2>
-            <ul>
-                <li>
-                    <a
-                        href="https://gitlab.com/tms-elte/backend-core/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {t('aboutPage.repositoriesLinkBackend')}
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://gitlab.com/tms-elte/frontend-react/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {t('aboutPage.repositoriesLinkFrontend')}
-                    </a>
-                </li>
-            </ul>
-            <h2>{t('aboutPage.documentation')}</h2>
-            <ul>
-                <li>
-                    <a
-                        href="https://tms-elte.gitlab.io/backend-core/phpdoc/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {t('aboutPage.documentationLinkBackend')}
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://tms-elte.gitlab.io/backend-core/phpdoc/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {t('aboutPage.documentationLinkFrontend')}
-                    </a>
-                </li>
-            </ul>
+            <p className="font-weight-bold">
+                {t('aboutPage.versionFrontend')}
+                : v
+                {process.env.REACT_APP_VERSION}
+                <br />
+                {t('aboutPage.versionBackend')}
+                : v
+                {publicSystemInfo.data?.version}
+            </p>
         </>
     );
 }
