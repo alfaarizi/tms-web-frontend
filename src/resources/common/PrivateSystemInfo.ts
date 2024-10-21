23731103352b44992b862991d1b866f1fbd23452
Gergő Kiszely <kiszely.geri@gmail.com>
2024-10-21 16:12:20 +0000

    fix: Web application tester default run time can be out of valid interval

--- src/resources/common/PrivateSystemInfo.ts
+++ src/resources/common/PrivateSystemInfo.ts
@@ -3,6 +3,7 @@ import { Semester } from './Semester';
 export interface PrivateSystemInfo {
     uploadMaxFilesize: number;
     postMaxSize: number;
+    maxWebAppRunTime: number;
     actualSemester: Semester,
     isAutoTestEnabled: boolean;
     isVersionControlEnabled: boolean;
